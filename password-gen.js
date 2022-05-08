995c0d424c69b56703a530ac7900b93f36ed20f1
r8drum <103894456+r8drum@users.noreply.github.com>
2022-05-08 16:34:21 +0200

    Add files via upload

---
+++ password-gen.js
@@ -0,0 +1,16 @@
+const { passwordStrength } = require('check-password-strength')
+
+
+function PassGen() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+[]{}|;:,./<>?~";
+  
+    for (var i = 0; i < 14; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+  }
+
+  var mypassword = PassGen()
+
+  console.log("Here you go -->  " + mypassword + " <-- Your password is " + passwordStrength(mypassword).value);
